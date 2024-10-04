696c219a0eb1287a2ca5f5654e284580d8440a98
Fraser Davidson <fraser@frontierhq.com>
2024-10-04 14:05:05 +0100

    WIP

--- src/terraform/ingest_pipeline.tf
+++
@@ -1,8 +0,0 @@
-resource "elasticstack_elasticsearch_ingest_pipeline" "main" {
-  for_each = { for i, v in local.ingest_pipelines : v.name => v }
-
-  name        = each.value.name
-  description = each.value.description
-
-  processors = each.value.processors
-}

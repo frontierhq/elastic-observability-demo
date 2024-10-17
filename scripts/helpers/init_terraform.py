de8c09ed7ae2290e7af0838a3e7ad04d7b7196b5
Fraser Davidson <fraser@frontierhq.com>
2024-10-17 14:40:46 +0100

    WIP

--- scripts/helpers/init_terraform.py
+++ scripts/helpers/init_terraform.py
@@ -1,13 +1,25 @@
-from python_terraform import Terraform
+from python_terraform import IsFlagged, Terraform
 
 
-def init_terraform(working_dir: str):
+def init_terraform(working_dir: str, workspace: str = None):
     terraform = Terraform(working_dir=working_dir)
 
     print("initialising terraform")
     return_code, _, _ = terraform.init()
     if (return_code != 0):
         exit(return_code)
+
+    if (workspace is not None):
+        print(f"selecting workspace {workspace}")
+        return_code, x, y = terraform.cmd(
+            "workspace select",
+            workspace,
+            capture_output=False,
+            no_color=IsFlagged,
+            or_create=IsFlagged,
+        )
+        if (return_code != 0):
+            exit(return_code)
 
     return terraform
 
